475b89ea5534aff7df83fb1908dbe63e1092e98c
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2023-10-30 19:09:05 +0530

    Add files via upload

---
+++ web development/web development/currency-converter/getConvert.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once('Converter.php');
+ if(isset($_POST['convert']))
+ {
+    $amount = $_POST['amount'];
+    $currency_from = $_POST['currency_from'];
+    $currency_to = $_POST['currency_to'];
+    $converter = new Converter();
+    $converter->setConvert($amount, $currency_from);
+    $result = $converter->getConvert($currency_to);
+    $out = array();
+    $out['amount'] = $amount;
+    $out['from'] = $currency_from;
+    $out['result'] = $result;
+    $out['to'] = $currency_to;
+    $_SESSION['value'] = $out;
+    header('location:index.php');
+ }
+else
+ {
+    header('location:index.php');
+ }
+?>
